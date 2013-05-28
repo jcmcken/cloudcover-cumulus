48ca6d8c5a3374ec601dcd42927d59712c4bc444
Jon McKenzie <jcmcken@gmail.com>
2013-05-28 09:26:27 -0400

    Remove auto-generated help message in tests

--- cumulus/tests/authorization/host.py
+++ cumulus/tests/authorization/host.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from tastypie.test import ResourceTestCase
 from cumulus.models import Host, Key, Datum
 from django.conf import settings
