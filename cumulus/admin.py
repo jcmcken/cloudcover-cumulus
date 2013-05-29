c51b139af18e672d9c88148ccd961da676372f0b
Jon McKenzie <jcmcken@gmail.com>
2013-05-28 22:02:50 -0400

    Fill out all 'batteries-included' functionality in admin interface

--- cumulus/admin.py
+++ cumulus/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from cumulus.models import Host, Key, Datum
 
+class DatumInline(admin.TabularInline):
+    model = Datum
+
 class HostAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'updated_at'
+    list_display = ('name', 'ip', 'created_at', 'updated_at')
+    ordering = ('-updated_at',)
+    search_fields = ('name', 'ip')
+    inlines = (DatumInline,)
 
 class KeyAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'updated_at'
+    list_display = ('name', 'type', 'created_at', 'updated_at')
+    list_filter = ('type',)
+    ordering = ('-updated_at',)
+    search_fields = ('name', 'type')
 
 class DatumAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'updated_at'
+    list_display = ('host', 'key', 'value', 'created_at', 'updated_at')
+    list_display_links = ('value',)
+    list_select_related = True
+    ordering = ('-updated_at',)
+    search_fields = ('value', 'host__name', 'key__type')
 
 admin.site.register(Host, HostAdmin)
 admin.site.register(Key, KeyAdmin)
