9ac320187a75b4f038eae69eeddc8bfc4a900edc
Jon McKenzie <jcmcken@gmail.com>
2013-05-29 02:18:27 -0400

    Show object IDs in admin interface

--- cumulus/admin.py
+++ cumulus/admin.py
@@ -6,21 +6,21 @@ class DatumInline(admin.TabularInline):
 
 class HostAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_at'
-    list_display = ('name', 'ip', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'ip', 'created_at', 'updated_at')
     ordering = ('-updated_at',)
     search_fields = ('name', 'ip')
     inlines = (DatumInline,)
 
 class KeyAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_at'
-    list_display = ('name', 'type', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'type', 'created_at', 'updated_at')
     list_filter = ('type',)
     ordering = ('-updated_at',)
     search_fields = ('name', 'type')
 
 class DatumAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_at'
-    list_display = ('host', 'key', 'value', 'created_at', 'updated_at')
+    list_display = ('id', 'host', 'key', 'value', 'created_at', 'updated_at')
     list_display_links = ('value',)
     list_select_related = True
     ordering = ('-updated_at',)
