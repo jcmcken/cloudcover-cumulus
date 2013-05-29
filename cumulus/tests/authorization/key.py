8e644a89f1646d1ff49a0d1fd569497252b3220f
Jon McKenzie <jcmcken@gmail.com>
2013-05-29 05:34:59 -0400

    Add key POST tests

--- cumulus/tests/authorization/key.py
+++ cumulus/tests/authorization/key.py
@@ -12,6 +12,11 @@ class KeyAuthorizationTest(ResourceTestCase):
         self.list_url = '/api/v1/key'
         self.superuser = 'admin'
         self.fakeuser = 'fakeuser'
+        self.post_data = {
+          'name': 'bacon',
+          'type': 'string',
+          'description': 'Bacon is delicious',
+        }
         settings.CUMULUS_SUPERUSERS = [self.superuser]
 
     def tearDown(self):
@@ -46,3 +51,23 @@ class KeyAuthorizationTest(ResourceTestCase):
         superuser_result = self.api_client.get(self.list_url, HTTP_REMOTE_USER=self.superuser)
         self.assertHttpOK(superuser_result)
         self.assertTrue(bool(self.deserialize(superuser_result)))
+
+    def _post(self, **kwargs):
+        return self.api_client.post(self.list_url, format='json', data=self.post_data,
+            **kwargs)
+    
+    # POST one, no user
+    def test_post_unauthorized_no_user(self):
+        self.assertHttpUnauthorized(self._post())
+
+    # POST one, unauthed user
+    def test_post_unauthorized_user(self):
+        self.assertHttpUnauthorized(
+          self._post(HTTP_REMOTE_USER=self.fakeuser)
+        )
+   
+    # POST one, superuser
+    def test_superuser_post_authorized(self):
+        self.assertHttpCreated(
+          self._post(HTTP_REMOTE_USER=self.superuser)
+        )
